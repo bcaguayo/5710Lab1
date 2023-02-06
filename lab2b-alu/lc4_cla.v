a3ca95fc454464c87e7f28dd8ba77a84886d1e82
BC Aguayo <saguayo@seas.upenn.edu>
2023-02-06 12:05:52 -0500

    gp4

--- lab2b-alu/lc4_cla.v
+++ lab2b-alu/lc4_cla.v
@@ -28,7 +28,16 @@ module gp4(input wire [3:0] gin, pin,
            input wire cin,
            output wire gout, pout,
            output wire [2:0] cout);
-   
+
+  assign pout = &pin;
+  assign gout = gin[3] | pin[3] & gin[2] | pin[3] & pin[2] & gin[1] |
+                pin[3] & pin[2] & pin[1] & gin[0];
+  
+  assign cout[0] = gin[0] | pin[0] & cin;
+  assign cout[1] = gin[1] | pin[1] & gin[0] | pin[1] & pin[0] & cin;
+  assign cout[2] = gin[2] | pin[2] & gin[1] | 
+                   pin[2] & pin[1] & gin[0] | pin[2] & pin[1] & pin[0] & cin;
+
 endmodule
 
 /**
