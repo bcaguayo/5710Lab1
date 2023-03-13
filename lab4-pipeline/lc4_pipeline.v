5b823051733bda234e7b83d2de060fb7d47d00b8
BC Aguayo <saguayo@seas.upenn.edu>
2023-03-13 18:35:48 -0400

    Pipelined Blocks (no bypass)

--- lab4-pipeline/lc4_pipeline.v
+++ lab4-pipeline/lc4_pipeline.v
@@ -63,14 +63,14 @@ module lc4_processor
 
    /// Register Values: PC
    wire [15:0] pc_D, insn_D;
-   Nbit_reg #(16, 16'h8200) D_pc_reg    (.in(pc),         .out(pc_D),   .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h8200) D_pc_reg    (.in(o_cur_pc),   .out(pc_D),   .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
    Nbit_reg #(16, 16'h0000) D_insn_reg  (.in(i_cur_insn), .out(insn_D), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
    
    //___________________________________DECODE___________________________________________
 
    wire [2:0] r1sel, r2sel, wsel;
    wire r1re, r2re, regfile_we, nzp_we, select_pc_plus_one, is_load, is_store, is_branch, is_control_insn;
-   lc4_decoder decoder(.insn(i_cur_insn), .r1sel(r1sel), .r1re(r1re), .r2sel(r2sel), .r2re(r2re), .wsel(wsel), 
+   lc4_decoder decoder(.insn(insn_D), .r1sel(r1sel), .r1re(r1re), .r2sel(r2sel), .r2re(r2re), .wsel(wsel), 
                        .regfile_we(regfile_we), .nzp_we(nzp_we), .select_pc_plus_one(select_pc_plus_one), .is_load(is_load), 
                        .is_store(is_store), .is_branch(is_branch), .is_control_insn(is_control_insn));
 
@@ -79,15 +79,16 @@ module lc4_processor
    
    // Ctrl_Insn: JSR, JSRR, TRAP
    // R7 = PC + 1: i_rd_we enable(1), i_rd/wsel = 111, i_wdata pc+1
-   wire irdwe = (i_cur_insn[15:12] == 4'h4 | i_cur_insn[15:12] == 4'hF) ? 1'b1 : regfile_we;
-   wire [2:0] ird = (i_cur_insn[15:12] == 4'h4 | i_cur_insn[15:12] == 4'hF) ? 3'b111 : wsel;
-   wire [15:0] iwdata = (i_cur_insn[15:12] == 4'h4 | i_cur_insn[15:12] == 4'hF) ? pc_plus_one : i_wdata;
+   wire        irdwe  = (insn_D[15:12] == 4'h4 | insn_D[15:12] == 4'hF) ? 1'b1 : regfile_we;
+   wire [2:0]  ird    = (insn_D[15:12] == 4'h4 | insn_D[15:12] == 4'hF) ? 3'b111 : wsel;
+   wire [15:0] iwdata = (insn_D[15:12] == 4'h4 | insn_D[15:12] == 4'hF) ? pc_plus_one : i_wdata;
 
    // Ctrl_Insn: RTI | PC = R7
-   wire [2:0] r1select = (i_cur_insn[15:12] == 4'b1000) ? 3'b111 : r1sel;
+   wire [2:0] r1select = (insn_D[15:12] == 4'b1000) ? 3'b111 : r1sel;
 
    // ********************** REGFILE
-   lc4_regfile #(.n(16)) register(.clk(clk),.rst(rst),.gwe(gwe),.i_rs(r1select),.o_rs_data(o_Rsdata),.i_rt(r2sel),.o_rt_data(o_Rtdata),.i_rd(ird),.i_wdata(iwdata),.i_rd_we(irdwe));
+   lc4_regfile #(.n(16)) register(.clk(clk), .rst(rst), .gwe(gwe),  .i_rs(r1select),  .o_rs_data(o_Rsdata), .i_rt(r2sel),
+                                  .o_rt_data(o_Rtdata), .i_rd(ird), .i_wdata(iwdata), .i_rd_we(irdwe));
 
 
    //______________________________X Pipeline Register___________________________________
@@ -102,7 +103,7 @@ module lc4_processor
    Nbit_reg #(16, 16'h0000) x_insn_reg  (.in(insn_D),   .out(insn_X),   .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
    Nbit_reg #(16, 16'h0000) x_rs_reg    (.in(o_Rsdata), .out(rsdata_X), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
    Nbit_reg #(16, 16'h0000) x_rt_reg    (.in(o_Rtdata), .out(rtdata_X), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   Nbit_reg #(16, 16'h0000) x_wdata_reg (.in(i_wdata),  .out(wdata_X),  .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) x_wdata_reg (.in(iwdata),   .out(wdata_X),  .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
 
    wire [2:0] r1sel_X, r2sel_X, wsel_X;
    wire r1re_X, r2re_X, regfile_we_X;
@@ -131,7 +132,7 @@ module lc4_processor
    // r2re_X && regfile_we_W && (r2sel_X == wsel_W)   ->    pass regInputMux into o_Rtdata
 
    wire[15:0] o_ALU;
-   lc4_alu alu (.i_insn(i_cur_insn), .i_pc(pc), .i_r1data(o_Rsdata), .i_r2data(o_Rtdata), .o_result(o_ALU));
+   lc4_alu alu (.i_insn(insn_X), .i_pc(pc_X), .i_r1data(rsdata_X), .i_r2data(rtdata_X), .o_result(o_ALU));
 
    //______________________________M Pipeline Register___________________________________
 
@@ -166,10 +167,13 @@ module lc4_processor
 
    //____________________________________MEMORY__________________________________________
 
-   assign o_dmem_we = is_store;
-   assign o_dmem_towrite = (is_load  == 1'b1) ? i_cur_dmem_data :
-                           (is_store == 1'b1) ? o_Rtdata : 16'd0; // DMwe=is_sw on lecture
-   assign o_dmem_addr = (is_load | is_store) ? o_ALU : 16'd0;
+   wire is_str = ctrl_sign_M[2];
+   wire is_ld  = ctrl_sign_M[3];
+
+   assign o_dmem_we = is_str;
+   assign o_dmem_towrite = (is_ld  == 1'b1) ? i_cur_dmem_data :
+                           (is_str == 1'b1) ? rtdata_M : 16'd0; // DMwe=is_sw on lecture
+   assign o_dmem_addr = (is_ld | is_str) ? alu_M : 16'd0;
 
    //______________________________W Pipeline Register___________________________________
 
@@ -203,17 +207,20 @@ module lc4_processor
    Nbit_reg #(16, 16'd0) W_alu_reg (.in(alu_M), .out(alu_W), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
    
    // new curr Memory
-   wire [15:0] dmem_W;
-   Nbit_reg #(16, 16'd0) W_dmem_reg (.in(i_cur_dmem_data), .out(dmem_W), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   wire dmem_we_W;
+   wire [15:0] dmem_towrite_W, dmem_addr_W;
+   Nbit_reg #(1, 1'd0)   W_dmem_we_reg   (.in(o_dmem_we),      .out(dmem_we_W),      .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'd0) W_dmem_towr_reg (.in(o_dmem_towrite), .out(dmem_towrite_W), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'd0) W_dmem_addr_reg (.in(o_dmem_addr),    .out(dmem_addr_W),    .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
 
    //__________________________________WRITEBACK_________________________________________
 
    // Reg Input Mux
 
-   // if load,  i_cur_dmem_data 
+   // if load,  o_dmem_towrite 
    // if str, put o_Rt_Data into memory
    wire [15:0] regInputMux;
-   wire [15:0] selected_from_DM = (is_load == 1'b1) ? i_cur_dmem_data : o_ALU;
+   wire [15:0] selected_from_DM = (is_load == 1'b1) ? dmem_towrite_W : o_ALU;
    assign regInputMux = (select_pc_plus_one == 16'd1) ? next_pc : selected_from_DM;
    assign i_wdata = regInputMux;
 
@@ -223,8 +230,8 @@ module lc4_processor
    wire [2:0] next_nzp = (i_wdata[15] == 1'b1) ? 3'b100 :
                          ($signed(i_wdata) == 16'h0000) ? 3'b010 : 3'b001;
    */
-   wire [2:0] next_nzp = ($signed(iwdata) == 16'd0) ? 3'b010 :
-                         (iwdata[15] ==  1'b1) ? 3'b100 : 3'b001;
+   wire [2:0] next_nzp = ($signed(wdata_W) == 16'd0) ? 3'b010 :
+                         (wdata_W[15] ==  1'b1) ? 3'b100 : 3'b001;
 
    // If CMP, CMPU, CMPI, CMPIU get ALU result
    // wire [2:0] nzp_mux = (i_cur_insn[15:12] == 4'b0010) ? alu_nzp : next_nzp;
@@ -276,17 +283,17 @@ module lc4_processor
    assign next_pc = is_control_insn? pc_ctrl_insn : pc_plus_one;
 
    // Set Test Wires Here 
-   assign test_stall          = 2'd0;          // Testbench: is this a stall cycle? (don't compare the test values)
-   assign test_cur_pc         = pc;             // Testbench: program counter
-   assign test_cur_insn       = i_cur_insn;     // Testbench: instruction bits
-   assign test_regfile_we     = regfile_we;     // Testbench: register file write enable
-   assign test_regfile_wsel   = wsel;           // Testbench: which register to write in the register file 
-   assign test_regfile_data   = iwdata;        // Testbench: value to write into the register file
-   assign test_nzp_we         = nzp_we;         // Testbench: NZP condition codes write enable
-   assign test_nzp_new_bits   = next_nzp;            // Testbench: value to write to NZP bits
-   assign test_dmem_we        = o_dmem_we;      // Testbench: data memory write enable
-   assign test_dmem_addr      = o_dmem_addr;    // Testbench: address to read/write memory
-   assign test_dmem_data      = o_dmem_towrite; // Testbench: value read/writen from/to memory
+   assign test_stall          = 2'd0;              // Testbench: is this a stall cycle? (don't compare the test values)
+   assign test_cur_pc         = pc_W;              // Testbench: program counter
+   assign test_cur_insn       = insn_W;            // Testbench: instruction bits
+   assign test_regfile_we     = regfile_data_W[0]; // Testbench: register file write enable
+   assign test_regfile_wsel   = wsel_W;            // Testbench: which register to write in the register file 
+   assign test_regfile_data   = wdata_W;           // Testbench: value to write into the register file
+   assign test_nzp_we         = nzp_we;            // Testbench: NZP condition codes write enable
+   assign test_nzp_new_bits   = next_nzp;          // Testbench: value to write to NZP bits
+   assign test_dmem_we        = dmem_we_W;         // Testbench: data memory write enable
+   assign test_dmem_addr      = dmem_addr_W;       // Testbench: address to read/write memory
+   assign test_dmem_data      = dmem_towrite_W;    // Testbench: value read/writen from/to memory
 
    // Test cases: test_alu, test_br, test_ld_br, test_mem, test_all, wireframe
 
@@ -298,7 +305,7 @@ module lc4_processor
    Regs and MX & Wx Bypassing? DONE
    Tests should pass by not breaking code? i guess
 
-   What do I put in test_stall ? Do I change it at all
+   What do I put in test_stall ?
 
    TODO:
    Wire In Pipeline Registers In Stages
