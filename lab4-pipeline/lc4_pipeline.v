5ed33ee908f6be4ce5f5be420409ff33f3b6e7dc
BC Aguayo <saguayo@seas.upenn.edu>
2023-03-13 20:08:39 -0400

    Monday

--- lab4-pipeline/lc4_pipeline.v
+++ lab4-pipeline/lc4_pipeline.v
@@ -43,9 +43,12 @@ module lc4_processor
    + 2: flushed due to misprediction or because the first real instruction hasn't made it through to the writeback stage yet
    + 3: stalled due to load-to-use penalty
    */
-   assign test_stall = 2'd2; 
+   
+   wire in_stall = 2'd0; 
    
    // only mx wx bypass for lab4a
+
+   // 16'h3000
 
    //____________________________________FETCH___________________________________________
 
@@ -63,8 +66,10 @@ module lc4_processor
 
    /// Register Values: PC
    wire [15:0] pc_D, insn_D;
-   Nbit_reg #(16, 16'h8200) D_pc_reg    (.in(o_cur_pc),   .out(pc_D),   .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   Nbit_reg #(16, 16'h0000) D_insn_reg  (.in(i_cur_insn), .out(insn_D), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   wire [1:0]  stall_D; 
+   Nbit_reg #( 2, 2'd2)     D_stall_reg (.in(in_stall),   .out(stall_D),  .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h8200) D_pc_reg    (.in(o_cur_pc),   .out(pc_D),     .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) D_insn_reg  (.in(i_cur_insn), .out(insn_D),   .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));   
    
    //___________________________________DECODE___________________________________________
 
@@ -76,15 +81,10 @@ module lc4_processor
 
    // Data Decode
    wire [15:0] o_Rsdata, o_Rtdata, i_wdata;
-   
-   // Ctrl_Insn: JSR, JSRR, TRAP
-   // R7 = PC + 1: i_rd_we enable(1), i_rd/wsel = 111, i_wdata pc+1
-   wire        irdwe  = (insn_D[15:12] == 4'h4 | insn_D[15:12] == 4'hF) ? 1'b1 : regfile_we;
-   wire [2:0]  ird    = (insn_D[15:12] == 4'h4 | insn_D[15:12] == 4'hF) ? 3'b111 : wsel;
-   wire [15:0] iwdata = (insn_D[15:12] == 4'h4 | insn_D[15:12] == 4'hF) ? pc_plus_one : i_wdata;
 
-   // Ctrl_Insn: RTI | PC = R7
-   wire [2:0] r1select = (insn_D[15:12] == 4'b1000) ? 3'b111 : r1sel;
+   wire irdwe;
+   wire [2:0]  ird, r1select;
+   wire [15:0] iwdata;   
 
    // ********************** REGFILE
    lc4_regfile #(.n(16)) register(.clk(clk), .rst(rst), .gwe(gwe),  .i_rs(r1select),  .o_rs_data(o_Rsdata), .i_rt(r2sel),
@@ -97,13 +97,16 @@ module lc4_processor
    // r1sel, r1re, r2sel, r2re, wsel, regfile_we
    // nzp_we, select_pc_plus_one, is_load, is_store, is_branch, is_control_insn
 
+   wire [1:0]  stall_X; 
+   Nbit_reg #( 2, 2'd2)     X_stall_reg (.in(stall_D),  .out(stall_X), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
    wire [15:0] pc_X, insn_X, rsdata_X, rtdata_X, wdata_X;
 
-   Nbit_reg #(16, 16'h8200) x_pc_reg    (.in(pc_D),     .out(pc_X),     .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   Nbit_reg #(16, 16'h0000) x_insn_reg  (.in(insn_D),   .out(insn_X),   .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   Nbit_reg #(16, 16'h0000) x_rs_reg    (.in(o_Rsdata), .out(rsdata_X), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   Nbit_reg #(16, 16'h0000) x_rt_reg    (.in(o_Rtdata), .out(rtdata_X), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-   Nbit_reg #(16, 16'h0000) x_wdata_reg (.in(iwdata),   .out(wdata_X),  .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h8200) X_pc_reg    (.in(pc_D),     .out(pc_X),     .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) X_insn_reg  (.in(insn_D),   .out(insn_X),   .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) X_rs_reg    (.in(o_Rsdata), .out(rsdata_X), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) X_rt_reg    (.in(o_Rtdata), .out(rtdata_X), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+   Nbit_reg #(16, 16'h0000) X_wdata_reg (.in(iwdata),   .out(wdata_X),  .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
 
    wire [2:0] r1sel_X, r2sel_X, wsel_X;
    wire r1re_X, r2re_X, regfile_we_X;
@@ -125,6 +128,10 @@ module lc4_processor
 
    // MUX o_Rsdata and o_Rtdata
 
+   // Bypass
+   // wire [15:0] rsdata;
+
+
    // r1sel_X == wsel_M         pass o_ALU into o_Rsdata
    // r2sel_X == wsel_M         pass o_ALU into o_Rtdata
 
@@ -140,6 +147,9 @@ module lc4_processor
    // r1sel, r1re, r2sel, r2re, wsel, regfile_we
    // nzp_we, select_pc_plus_one, is_load, is_store, is_branch, is_control_insn
    // o_ALU
+
+   wire [1:0]  stall_M; 
+   Nbit_reg #( 2, 2'd2)     M_stall_reg (.in(stall_X),  .out(stall_M),    .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
 
    wire [15:0] pc_M, insn_M, rsdata_M, rtdata_M, wdata_M;
 
@@ -183,6 +193,9 @@ module lc4_processor
    // o_ALU
    // i_cur_dmem_data
 
+   wire [1:0]  stall_W; 
+   Nbit_reg #( 2, 2'd2)     W_stall_reg (.in(stall_M),  .out(stall_W),    .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
    wire [15:0] pc_W, insn_W, rsdata_W, rtdata_W, wdata_W;
 
    Nbit_reg #(16, 16'h8200) W_pc_reg    (.in(pc_M),     .out(pc_W),       .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
@@ -219,9 +232,13 @@ module lc4_processor
 
    // if load,  o_dmem_towrite 
    // if str, put o_Rt_Data into memory
+
+   // propagate next pc
+   wire sel_pc_plus_one = ctrl_sign_W[4];
+
    wire [15:0] regInputMux;
    wire [15:0] selected_from_DM = (is_load == 1'b1) ? dmem_towrite_W : o_ALU;
-   assign regInputMux = (select_pc_plus_one == 16'd1) ? next_pc : selected_from_DM;
+   assign regInputMux = (sel_pc_plus_one == 16'd1) ? next_pc : selected_from_DM;
    assign i_wdata = regInputMux;
 
    // NZP Register
@@ -247,49 +264,60 @@ module lc4_processor
    wire [15:0] pc_ctrl_insn;
 
    // Compute PC Plus One
-   wire [15:0] s_ext_br = {{7{i_cur_insn[8]}}, i_cur_insn[8:0]};
+   wire [15:0] s_ext_br = {{7{insn_W[8]}}, insn_W[8:0]};
 
    // Branch is taken check NZP vs insn[11:9]
-   wire take_br = (nzp[2] & (i_cur_insn[11] == 1'b1)) ? 1'b1 :
-                  (nzp[1] & (i_cur_insn[10] == 1'b1)) ? 1'b1 :
-                  (nzp[0] & (i_cur_insn[9] == 1'b1)) ? 1'b1 : 1'b0;
+   wire take_br = (nzp[2] & (insn_W[11] == 1'b1)) ? 1'b1 :
+                  (nzp[1] & (insn_W[10] == 1'b1)) ? 1'b1 :
+                  (nzp[0] & (insn_W[9]  == 1'b1)) ? 1'b1 : 1'b0;
 
    // If Branch that isn't NOP, add IMM9
    wire [15:0] b_in = (is_branch & take_br)? s_ext_br : 16'd0;
    cla16 cla_branch(.a(pc),.b(b_in),.cin(1'b1),.sum(pc_plus_one));   
    
    // JSR & JSRR  -------------------------------------------------------------
-   wire [15:0] s_ext_jsr = {i_cur_insn[10], i_cur_insn[10:0], 4'b0}; // JSR Sign Ext
-   wire [15:0] pc_jsr = (pc & 16'h8000) | s_ext_jsr;   // JSR PC
-   wire [15:0] pc_jsrr = (i_cur_insn[11] == 1'b1) ? pc_jsr : o_Rsdata;   // JSR or JSRR
+   wire [15:0] s_ext_jsr = {insn_W[10], insn_W[10:0], 4'b0}; // JSR Sign Ext
+   wire [15:0] pc_jsr    = (pc & 16'h8000) | s_ext_jsr;   // JSR PC
+   wire [15:0] pc_jsrr   = (insn_W[11] == 1'b1) ? pc_jsr : o_Rsdata;   // JSR or JSRR
 
    // JMP & JMPR  -------------------------------------------------------------
    wire [15:0] pc_jmp;
-   wire [15:0] s_ext_jmp = {{5{i_cur_insn[10]}}, i_cur_insn[10:0]};     // JMP Sign Ext
+   wire [15:0] s_ext_jmp = {{5{insn_W[10]}}, insn_W[10:0]};     // JMP Sign Ext
    cla16 cla_jmp(.a(pc),.b(s_ext_jmp),.cin(1'b1),.sum(pc_jmp));       // JMP
-   wire [15:0] pc_jmpr = (i_cur_insn[11] == 1'b1) ? pc_jmp : o_Rsdata;     // JMP or JMPR
+   wire [15:0] pc_jmpr = (insn_W[11] == 1'b1) ? pc_jmp : o_Rsdata;     // JMP or JMPR
 
    // TRAP  -------------------------------------------------------------------
-   wire[15:0] pc_trap = {8'h80, i_cur_insn[7:0]};
+   wire[15:0] pc_trap = {8'h80, insn_W[7:0]};
+
+   // Ctrl_Insn: JSR, JSRR, TRAP
+   // R7 = PC + 1: i_rd_we enable(1), i_rd/wsel = 111, i_wdata pc+1
+   // insn_D is bad?
+   wire        irdwe  = (insn_W[15:12] == 4'h4 | insn_W[15:12] == 4'hF) ? 1'b1 : regfile_we;
+   wire [2:0]  ird    = (insn_W[15:12] == 4'h4 | insn_W[15:12] == 4'hF) ? 3'b111 : wsel;
+   wire [15:0] iwdata = (insn_W[15:12] == 4'h4 | insn_W[15:12] == 4'hF) ? pc_plus_one : i_wdata;
 
    // RTI  --------------------------------------------------------------------
    wire[15:0] pc_rti = o_Rsdata; // Selected to be Rs above
 
-   // Assign
-   assign pc_ctrl_insn = (i_cur_insn[15:12] == 4'h4) ? pc_jsrr :
-                         (i_cur_insn[15:12] == 4'hC) ? pc_jmpr :
-                         (i_cur_insn[15:12] == 4'hF) ? pc_trap : pc_rti;
+   // Ctrl_Insn: RTI | PC = R7
+   wire [2:0] r1select = (insn_D[15:12] == 4'b1000) ? 3'b111 : r1sel;   
+
+
+   // Assign  -----------------------------------------------------------------
+   assign pc_ctrl_insn = (insn_W[15:12] == 4'h4) ? pc_jsrr :
+                         (insn_W[15:12] == 4'hC) ? pc_jmpr :
+                         (insn_W[15:12] == 4'hF) ? pc_trap : pc_rti;
 
    assign next_pc = is_control_insn? pc_ctrl_insn : pc_plus_one;
 
-   // Set Test Wires Here 
-   assign test_stall          = 2'd0;              // Testbench: is this a stall cycle? (don't compare the test values)
+   // Set Test Wires Her
+   assign test_stall          = stall_W;           // Testbench: is this a stall cycle? (don't compare the test values)
    assign test_cur_pc         = pc_W;              // Testbench: program counter
    assign test_cur_insn       = insn_W;            // Testbench: instruction bits
    assign test_regfile_we     = regfile_data_W[0]; // Testbench: register file write enable
    assign test_regfile_wsel   = wsel_W;            // Testbench: which register to write in the register file 
    assign test_regfile_data   = wdata_W;           // Testbench: value to write into the register file
-   assign test_nzp_we         = nzp_we;            // Testbench: NZP condition codes write enable
+   assign test_nzp_we         = ctrl_sign_W[5];    // Testbench: NZP condition codes write enable
    assign test_nzp_new_bits   = next_nzp;          // Testbench: value to write to NZP bits
    assign test_dmem_we        = dmem_we_W;         // Testbench: data memory write enable
    assign test_dmem_addr      = dmem_addr_W;       // Testbench: address to read/write memory
@@ -311,6 +339,8 @@ module lc4_processor
    Wire In Pipeline Registers In Stages
    Wire In Pipeline Registers To TestBench
    Implement Bypass (logic done)
+
+   CHECK WHERE EVERY WIRE IS COMING FROM
    */
 
    /* Add $display(...) calls in the always block below to
